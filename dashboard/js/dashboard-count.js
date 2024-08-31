2d02c1025ef6b44134948481595cdf80d18c5d65
squirrel-cage-work <87857140+squirrel-cage-work@users.noreply.github.com>
2024-08-31 21:42:06 +0900

    Update dashboard-count.js

--- dashboard/js/dashboard-count.js
+++ dashboard/js/dashboard-count.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
 
+    const apiUrl = config.apiUrl;
+
+    const userCountElement = document.getElementById('totalswitches');
+
+    // APIからデータを取得
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // 取得したユーザー数を表示
+            userCountElement.textContent = data.totalswitches;
+        })
+        .catch(error => {
+            console.error('エラーが発生しました:', error);
+            userCountElement.textContent = 'データを取得できませんでした';
+        });
+});
