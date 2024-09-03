d76740a8fa0a42573892d6c35c4aae90dbe3a461
squirrel-cage-work <87857140+squirrel-cage-work@users.noreply.github.com>
2024-09-03 22:57:19 +0900

    Update netgraph-devices-function.py

--- backend/lambda/netgraph-devices-function.py
+++ backend/lambda/netgraph-devices-function.py
@@ -1,15 +1,22 @@
 import json
 import boto3
+import logging
 from neo4j  import GraphDatabase, basic_auth
+
+logger = logging.getLogger()
 
 def string_to_unicode(s):
     return '-'.join(str(ord(c)) for c in s)
 
 def lambda_handler(event, context):
 
-    deviceType = event['pathParameters']['deviceType']
+    logger.info(event)
+
     deviceName = event['pathParameters']['deviceName']
     nodeParam  = string_to_unicode(deviceName)
+
+    ''' neo4j login parameter from ssm parameter store
+    '''
 
     ssm = boto3.client('ssm')
     responses = ssm.get_parameters(
@@ -28,16 +35,27 @@ def lambda_handler(event, context):
         if response['Name'] == 'neo4j-password':
             neo4j_password = response['Value']
 
+    ''' 
+    '''
     driver = GraphDatabase.driver(neo4j_url, auth=basic_auth(neo4j_username, neo4j_password))
     session = driver.session()
     
     print(nodeParam)
 
-    if deviceType == 'switches':
-        if event['httpMethod'] == 'POST':
-            query = '''
-            CREATE (s$nodeParam:Switch {name: $deviceName})
-            '''
+    if event['httpMethod'] == 'GET':
+        query = '''
+        '''
+    elif event['httpMethod'] == 'POST':
+        query = '''
+        CREATE (s$nodeParam:Switch {name: $deviceName})
+        '''
+    elif event['httpMethod'] == 'PUT':
+        query = '''
+        '''
+    elif event['httpMethod'] == 'DELETE':
+        query = '''
+        '''
+        
             
     parameters = {
         'nodeParam': nodeParam,
@@ -49,6 +67,13 @@ def lambda_handler(event, context):
     for record in results:
         print(record)
 
+    ''' Response Body
+    ''' 
+    '''
+    response_body = {
+        'tenantName': tenantName
+    }
+    '''
 
     # TODO implement
     return {
