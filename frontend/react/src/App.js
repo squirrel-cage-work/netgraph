fa8030cdafee91496a1098f502c5ed116eee21a7
squirrel <squirrel>
2024-09-29 23:17:04 +0900

    update App.js

--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu';
 import Tenant from './Tenant';
 import Switch from './Switch';
+import Vlan from './Vlan';
 import DeviceRouter from './DeviceRouter';
 import AddItem from './AddItem';
 import Component from './Component';
@@ -19,6 +20,7 @@ function App() {
               <Route path="/tenant" element={<Tenant />} />
               <Route path="/switch" element={<Switch />} />
               <Route path="/router" element={<DeviceRouter />} />
+              <Route path="/vlan" element={<Vlan />} />
               {/* 他のルートもここに追加する */}
               <Route path="/additem" element={<AddItem />} />
               <Route path="/component" element={<Component />} />
