06d84f9a7094cd647b4df55bd2f347da47a496e6
squirrel <squirrel>
2024-09-29 23:16:48 +0900

    create Vlan.js

---
+++ frontend/react/src/Vlan.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TitleHeader from './components/TitleHeader';
+//import CreateForm from './components/CreateForm';
+import CommonTable from './components/CommonTable';
+import CardGrid from './components/CardGrid';
+
+function Vlan() {
+  return (
+    <div className="flex">
+      <main className="w-full bg-slate-200 px-6">
+        <div>
+          <TitleHeader title="Vlan" />
+        </div>
+        <div className="w-full bg-white rounded-md border-1 border-gray-400 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1">
+          <CardGrid type="vlans"/>
+        </div>
+        <div className='py-6'>
+          <CommonTable type="vlans" />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Vlan;
