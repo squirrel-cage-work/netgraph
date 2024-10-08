b66c6adfee37ff77d206ccecff9ab9cfed932cd0
squirrel <squirrel>
2024-10-08 15:25:00 +0900

    create netgraph-devices-function.py

--- backend/lambda/netgraph-devices-function.py
+++ backend/lambda/netgraph-devices-function.py
@@ -1,62 +1,106 @@
 import json
 import boto3
 import logging
+import time
 from neo4j  import GraphDatabase, basic_auth
 
 logger = logging.getLogger()
 
+''' get parameters to access neo4j from ssm parameter store
+'''
+ssm = boto3.client('ssm')
+
+responses = ssm.get_parameters(
+    Names=[
+        'neo4j-url',
+        'neo4j-username',
+        'neo4j-password'
+    ],
+    WithDecryption=True
+)
+
+for response in responses['Parameters']:
+    if response['Name'] == 'neo4j-url':
+        neo4j_url = response['Value']
+    if response['Name'] == 'neo4j-username':
+        neo4j_username = response['Value']
+    if response['Name'] == 'neo4j-password':
+        neo4j_password = response['Value']
+
+'''  create driver
+'''
+driver = GraphDatabase.driver(neo4j_url, auth=basic_auth(neo4j_username, neo4j_password))
+
 def lambda_handler(event, context):
-    
+
     devices    = event['pathParameters']['devices']
 
     ''' neo4j login parameter from ssm parameter store
     '''
 
-    ssm = boto3.client('ssm')
-    responses = ssm.get_parameters(
-        Names=[
-            'neo4j-url',
-            'neo4j-username',
-            'neo4j-password'
-        ],
-        WithDecryption=True
-    )
-
-    for response in responses['Parameters']:
-        if response['Name'] == 'neo4j-url':
-            neo4j_url = response['Value']
-        if response['Name'] == 'neo4j-username':
-            neo4j_username = response['Value']
-        if response['Name'] == 'neo4j-password':
-            neo4j_password = response['Value']
-
     response = []
     
     try:
-        driver = GraphDatabase.driver(neo4j_url, auth=basic_auth(neo4j_username, neo4j_password))
-        session = driver.session()
-    
-        if devices == 'switches':
-            if event['httpMethod'] == 'GET':
-                query = '''
-                MATCH (n:Switch) RETURN n
-                '''
-                
-        results = session.run(query)
-    
-        for record in results:
-            name   = record['n']['name']
-            tenant = record['n']['Tenant']
+        with driver.session() as session:
 
-            body   = {
-                "deviceName": name,
-                "properties": {
-                    "tenant": tenant
-                    }
-            }
+            if devices == 'switches':
+                if event['httpMethod'] == 'GET':
+                    query = '''
+                    MATCH (n:Switch) RETURN n
+                    '''
+                    results = session.run(query)
+                
+                    for record in results:
+                        name   = record['n']['name']
+                        tenant = record['n']['Tenant']
+
+                        body   = {
+                            "deviceName": name,
+                            "properties": {
+                                "tenant": tenant
+                                }
+                            }
             
-            response.append(body)
+                        response.append(body)
             
+            elif devices == 'tenants':
+                if event['httpMethod'] == 'GET':
+                    query = '''
+                    MATCH (n:Tenant) RETURN n
+                    '''            
+                
+                    results = session.run(query)
+                
+                    for record in results:
+                        name   = record['n']['name']
+
+                        body   = {
+                            "deviceName": name,
+                            "properties": {
+                                }
+                            }
+            
+                        response.append(body)
+
+            elif devices == 'routers':
+                if event['httpMethod'] == 'GET':
+                    query = '''
+                    MATCH (n:Router) RETURN n
+                    '''            
+                
+                    results = session.run(query)
+                
+                    for record in results:
+                        name   = record['n']['name']
+
+                        body   = {
+                            "deviceName": name,
+                            "properties": {
+                                }
+                            }
+            
+                        response.append(body)
+    
     except Exception as e:
         logging.error("Error occurred: %s", str(e))
         response = {
@@ -70,10 +114,7 @@ def lambda_handler(event, context):
         }
         return response 
         
-    finally:
-        session.close()
-        driver.close()
-    
+
     # TODO implement
     return {
         'statusCode': 200,
